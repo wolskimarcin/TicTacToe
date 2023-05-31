44808dbca56af2c753b91e68658b8ca2485b17a5
wolskimarcin <wolski.marcin.it@gmail.com>
2023-05-31 17:48:51 +0200

    Change game result dialog styling and center player selection

--- app/src/main/java/com/busher/tictactoe/components/BaseDialog.java
+++ app/src/main/java/com/busher/tictactoe/components/BaseDialog.java
@@ -3,10 +3,12 @@ package com.busher.tictactoe.components;
 import android.app.Dialog;
 import android.content.Context;
 
+import com.busher.tictactoe.R;
+
 public abstract class BaseDialog extends Dialog {
 
     public BaseDialog(Context context) {
-        super(context);
+        super(context, R.style.RoundedCornersDialog);
     }
 
     protected abstract void initializeViews();
